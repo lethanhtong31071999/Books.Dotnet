ae342a4756d00d5fb7d51af65f3a733097d8d7fd
Le Thanh Tong <lethanhtong31071999@gmail.com>
2022-12-09 17:41:19 +0700

    CATEGORY-CRUD-MVC

--- Books/Controllers/CategoryController.cs
+++ Books/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Books.Data;
 using Books.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Books.Controllers
 {
@@ -17,9 +18,73 @@ namespace Books.Controllers
             return View(categories);
         }
 
+        //Create
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name cannot exactly match with Display Order");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category was deleted successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Update
+        public IActionResult Update(int? id)
+        {
+            if(id == null || id == 0) return NotFound();
+            var model = _db.Categories.AsNoTracking().FirstOrDefault<Category>(obj => obj.Id == id);
+            if(model == null) return NotFound();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Category c)
+        {
+            if (c == null) return View(c);
+            _db.Categories.Update(c);
+            _db.SaveChanges();
+            TempData["success"] = "Category was updated successfully!";
+            return RedirectToAction("Index", "Category");
+        }
+
+        // Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null) return NotFound();
+            var obj = _db.Categories.FirstOrDefault<Category>(c => c.Id == id);
+            if (obj != null) return View(obj);
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int? id, bool none = true)
+        {
+            if (id == 0 || id == null) return NotFound();
+            var obj = _db.Categories.FirstOrDefault<Category>(c => c.Id == id);
+            if (obj != null)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category was deleted successfully!";
+                return RedirectToAction("Index", "Category");
+            }
+            return NotFound();
         }
     }
 }
--- Books/Models/Category.cs
+++ Books/Models/Category.cs
@@ -8,6 +8,9 @@ namespace Books.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
+        [Display(Name = "Display Order")]
+        [Range(1,100, ErrorMessage = "Range is from 1 to 100")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
