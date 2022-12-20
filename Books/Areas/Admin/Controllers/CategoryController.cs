d256685177fd4ce580e0a0a914015a3ea026127c
Le Thanh Tong <lethanhtong31071999@gmail.com>
2022-12-20 16:02:50 +0700

    SeparateAreas

--- Books/Areas/Admin/Controllers/CategoryController.cs
+++ Books/Areas/Admin/Controllers/CategoryController.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Books.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unit;
